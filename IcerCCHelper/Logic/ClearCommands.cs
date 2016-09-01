274f6a046c318b55e1a30659f9a4e282a0f26e1c
Icer <liangshuangde@163.com>
2016-09-01 14:03:40 +0000

    add qoutes to clear commands

--- IcerCCHelper/Logic/ClearCommands.cs
+++ IcerCCHelper/Logic/ClearCommands.cs
@@ -10,7 +10,7 @@ namespace IcerDesign.CCHelper
         public static ClearCommand[] CheckInFiles(string comment, string[] files)
         {
             var lst = files
-                .Select(s => String.Format(@"checkin -ide -c ""{1}"" ""{0}""", s, comment))
+                .Select(file => $@"checkin -ide -c ""{comment}"" ""{file}""")
                 .Select(s => new ClearCommand(s, new ElementPath(s).Parent))
                 .ToArray();
 
@@ -20,7 +20,7 @@ namespace IcerDesign.CCHelper
         public static ClearCommand[] ApplyLabelToFiles(string label, string[] files)
         {
             var lst = files
-                .Select(s => String.Format(@"mklabel -replace {1} ""{0}""", s, label))
+                .Select(file => $@"mklabel -replace ""{label}"" ""{file}""")
                 //.Select(s => string.Format(@"reqmaster {0}@@/main&cleartool mklabel -replace {1} {0}", s, label))
                 .Select(s => new ClearCommand(s, new ElementPath(s).Parent))
                 .ToArray();
@@ -31,7 +31,7 @@ namespace IcerDesign.CCHelper
         public static ClearCommand[] UndoCheckOut(string[] files)
         {
             var lst = files
-                .Select(s => String.Format(@"unco -rm ""{0}""", s))
+                .Select(file => $@"unco -rm ""{file}""")
                 .Select(s => new ClearCommand(s, new ElementPath(s).Parent))
                 .ToArray();
 
@@ -42,12 +42,12 @@ namespace IcerDesign.CCHelper
         {
             var ret = new ElementPath(filename);
             var cmds = new List<ClearCommand>();
-            cmds.Add(new ClearCommand("reqmaster " + filename + "@@/main", ret.Parent, (ex) =>
+            cmds.Add(new ClearCommand($@"reqmaster ""{filename}@@/main""", ret.Parent, (ex) =>
             {
                 if (ex.Message.IndexOf("The object is already mastered by replica") == -1) throw ex;
             }));
 
-            cmds.Add(new ClearCommand("co -c \"" + comment + @""" " + filename, ret.Parent, (ex) =>
+            cmds.Add(new ClearCommand($@"co -c ""{comment}"" ""{filename}""", ret.Parent, (ex) =>
             {
                 if (ex.Message.IndexOf("is already checked out to view") == -1) throw ex;
             }));
@@ -67,7 +67,7 @@ namespace IcerDesign.CCHelper
             if (File.Exists(path))
             {
                 commands.AddRange(CheckOutFile("", parent));
-                commands.Add(new ClearCommand("mkelem -master -nc " + element, parent));
+                commands.Add(new ClearCommand($@"mkelem -master -nc ""{element}""", parent));
                 eles.Add(path);
                 totalNumber++;
             }
@@ -78,17 +78,13 @@ namespace IcerDesign.CCHelper
                 foreach (var dir in directories)
                 {
                     var di = new DirectoryInfo(dir);
-                    commands.Add(new ClearCommand(
-                        string.Format("mkelem -mkpath -master -nc \"{0}\"", di.Name),
-                        di.Parent.FullName));
+                    commands.Add(new ClearCommand($@"mkelem -mkpath -master -nc ""{di.Name}""", di.Parent.FullName));
                     if (eles.Count < elementLimit) eles.Add(Path.Combine(di.Parent.FullName, di.Name));
                     totalNumber++;
                     var files = Directory.GetFiles(dir, "*", SearchOption.TopDirectoryOnly);
                     foreach (var file in files)
                     {
-                        commands.Add(new ClearCommand(
-                            string.Format("mkelem -mkpath -master -nc \"{0}\"", file),
-                            dir));
+                        commands.Add(new ClearCommand($@"mkelem -mkpath -master -nc ""{file}""", dir));
                         if (eles.Count < elementLimit) eles.Add(file);
                         totalNumber++;
                     }
@@ -108,7 +104,7 @@ namespace IcerDesign.CCHelper
             var commands = new List<ClearCommand>();
             var ret = new ElementPath(path);
             commands.AddRange(CheckOutFile("", ret.Parent));
-            commands.Add(new ClearCommand(string.Format("rmname \"{0}\"", ret.Element), ret.Parent, (ex) =>
+            commands.Add(new ClearCommand($@"rmname ""{ret.Element}""", ret.Parent, (ex) =>
             {
                 if (ex.Message.IndexOf("Not a vob object") == -1) throw ex;
             }));
